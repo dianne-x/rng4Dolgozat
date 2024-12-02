d38a9206acc9a6d85f7afbf04f218db4762962f0
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2024-12-02 09:29:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("How many questions? ");
+        if (int.TryParse(Console.ReadLine(), out int numberOfQuestions))
+        {
+            Random random = new Random();
+            for (int i = 0; i < numberOfQuestions; i++)
+            {
+                bool answer = random.Next(2) == 0;
+                Console.WriteLine(answer ? "Yes" : "No");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+    }
+}
